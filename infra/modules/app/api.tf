156e7d2d842a8e4c43bd8d081b83275d947db782
Markus Ryöti <mryoti@gmail.com>
2025-09-22 19:24:57 +0300

    better startup script

--- infra/modules/app/api.tf
+++ infra/modules/app/api.tf
@@ -9,11 +9,29 @@ resource "aws_instance" "api" {
     #!/bin/bash
     set -ex
 
-    sudo apt update -y
-    sudo apt install -y docker.io
+    sudo apt-get -y update
+    sudo apt-get -y install ca-certificates curl gnupg lsb-release
+
+    sudo install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+      https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    sudo apt-get -y update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
     sudo systemctl enable docker
     sudo systemctl start docker
+
+    until sudo docker info >/dev/null 2>&1; do
+      echo "Waiting for Docker to start..."
+      sleep 5
+    done
 
     sudo docker run -d -p 8080:8080 --name myapp ${var.api_container_name}
 
